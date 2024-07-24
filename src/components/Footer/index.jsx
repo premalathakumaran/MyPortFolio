35f713c2a0a17b07b1dc96dd6da10fec0fb12bbe
premalathakumaran <premalathakumaran18@gmail.com>
2024-07-24 15:10:07 +0530

    profile commit

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -91,7 +91,7 @@ function Footer() {
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
-          <NavLink href="#experience">Experience</NavLink>
+          <NavLink href="#experience">Internship</NavLink>
           <NavLink href="#projects">Projects</NavLink>
           <NavLink href="#education">Education</NavLink>
         </Nav>
